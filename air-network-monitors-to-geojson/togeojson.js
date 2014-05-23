c939fe9af4f016b81b1c7fc600187becb8ace865
MappingKat <katrina@engelsted.co>
2014-05-23 12:06:44 -0600

    updating script for networks

--- air-network-monitors-to-geojson/togeojson.js
+++ air-network-monitors-to-geojson/togeojson.js
@@ -1,68 +1,30 @@
 
 var fs = require('fs'),
-request = require('request'),
-url = 'http://views.cira.colostate.edu/fed/services/GetResults.aspx?action=getqueryresultsasjsonstring&cmdid=AirQualitySitesByDataset1&cmdfileid=ServiceSqlCommandFile&dsidse=10001';
+filename = 'data/monitoring_sites.json';
 
-request(url + '?format=json', function(error, response, body){
-  if (!error && response.statusCode === 200) {
-    var data = JSON.parse(response.body);
-    console.log(data[0]);
-    console.log(data);
+fs.readFile(filename, function(err, data) {
+  if (err) throw err;
 
-    stream.once('open', function() {
-      stream.write('callback(' + JSON.stringify(bounds) + ');');
-      stream.end();
-    }
+  var info = [],
+  records = JSON.parse(data);
+
+  for (var i = 0; i < records.length; i++) {
+    var record = records[i];
+
+    info.push({
+      'y': parseFloat(record.Longitude),
+      'x': parseFloat(record.Latitude),
+      'i': record.ImNetworkID,
+      'c': record.DatasetCodes,
+      'd': record.DataAccess,
+      'e': parseInt(record.ElevationMSL, 10),
+      't': record.DataEndDate,
+      'p': record.Parameter,
+      'z': record.SiteCode,
+      'a': record.SiteName,
+      's': record.DataStartDate
+    });
   }
+
+  fs.writeFile('data/air-sites.json', JSON.stringify(info));
 });
-
-//     stream.once('open', function() {
-//       stream.write('callback(' + JSON.stringify(bounds) + ');');
-//       stream.end();
-//     });
-//   } else {
-//     console.log(response.statusCode);
-    // }
-
-// fs.readFile('data/network-monitors.json', function(error, data) {
-//   var geometries = [],
-//       stream = fs.createWriteStream('data/network-monitors.geojson'),
-//       records = JSON.parse(data);
-
-//   for (var i = 0; i < records.length; i++) {
-//     var record = records[i];
-
-//     geometries.push({
-//       'geometry': {
-//         'coordinates': [
-//           parseFloat(record.Lon),
-//           parseFloat(record.Lat)
-//         ],
-//         'type': 'Point',
-//       },
-//       'properties': {
-        // 'County FIPS': record.CountyFIPS,
-        // 'Country': record.Country,
-        // 'Dataset Code': record.DatasetCode,
-        // 'Dataset ID': record.DatasetID,
-        // 'Dataset Name': record.DatasetName,
-        // 'Elevation': parseInt(record.Elev, 10),
-        // 'End Date': new Date(record.End),
-        // 'EPA Code': record.EPACode,
-        // 'Frequency': record.Freq,
-        // 'Site Code': record.SiteCode,
-        // 'Site ID': record.SiteID,
-        // 'Site Name': record.SiteName,
-        // 'Start Date': new Date(record.Start),
-        // 'State': record.State,
-//         'State FIPS': record.StateFIPS
-//       },
-//       'type': 'Feature'
-//     });
-//   }
-
-//   stream.once('open', function() {
-//     stream.write('{"type":"FeatureCollection","features":' + JSON.stringify(geometries) + '}');
-//     stream.end();
-//   });
-// });
